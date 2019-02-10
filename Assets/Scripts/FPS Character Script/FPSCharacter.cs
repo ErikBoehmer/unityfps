3c1b8f86aef152f6759c50c65c1ef5d322aaaef5
ErikBoehmer <43480405+ErikBoehmer@users.noreply.github.com>
2019-02-10 13:50:02 -0600

    Updated Version of Game Homework2

---
+++ Assets/Scripts/FPS Character Script/FPSCharacter.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSController : MonoBehaviour
+{
+
+    private Transform firstperson_view;
+    private Transform firsperson_camera;
+    private Vector3 firstperson_view_rotation = Vector3.zero;
+
+    public float walkSpeed = 6.75;
+    public float runSpeed = 10f;
+    public float crouchSpeed = 4f;
+    public float jumpSpeed = 8f;
+    public float gravity = 20f;
+
+    private float Speed;
+
+    private bool is_Moving, is_Grounded; is_Crouching;
+    private float inputX, inputY;
+    private float inputX_Set, inputY_Set;
+    private float inputModifyFactor;
+    private bool limitDiagionalSpeed = true;
+    private float antiBumpFactor = 0.75f;
+    private Vector3 moveDirection = 0f;
+
+
+
+
+
+
+    // Start is called before the first frame update
+
+
+
+
+    void Start()
+    {
+        firstperson_view = transform.Find("FPS View").transform;
+        charController = GetComponent<CharacterController>();
+        Speed = walkSpeed;
+        is_Moving = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+
+    }
+    void PlayerMovement()
+    {
+        if(Input.GetKey(KeyCode.W) || Input.GetKey.S)){
+            if(Input.GetKey(KeyCode.W))
+            {
+                inputY_Set = 1f;
+            }
+            else
+            {
+                inputY_Set = -1f;
+            }
+            else
+            {
+                inputY_Set = 0f;
+            }
+            if(input.GetKey(KeyCode.A) || input.GetKey(KeyCode.D))
+            {
+                if (input.GetKey(KeyCode.A))
+                {
+                    inputX_Set = -1f;
+                }
+                else
+                {
+                    inputX_Set = 1f;
+                }
+                else
+                {
+                    inputX_Set = 0f;
+                }
+
+
+            }
+
+            inputY = Mathf.Lerp(inputY, inputY_Set, Time.deltaTime * 19f);
+            inputX = Mathf.Lerp(inputY, inputX_Set, Time.deltaTime * 19f);
+
+            inputModifyFactor = Mathf.Lerp(inputModifyFactor, (inputY_Set != 0 && inputX_Set != 0 && limitDiagionalSpeed) ? 0.75f : 1.0f,
+                Time.deltaTime * 19f);
+
+
+            firstperson_view_rotation = Vector3.Lerp(firstperson_view_rotation, Vector3.zero, Time.deltaTime * 5f);
+            firstperson_view.localEulerAngles = firstperson_view_rotation; 
+
+            if(is_Grounded)
+            {
+                moveDirection = new Vector3(inputX * inputModifyFactor, -antibumpfactor, inputY * inputModifyFactor);
+                moveDirection = transform.TransformDirection(moveDirection) * Speed;
+
+            }
+
+            moveDirection.y -= gravity * Time.deltaTime;
+
+            is_Grounded = (charController.Move(moveDirection * Time.deltaTime) & CollisionFlags.Below) !=0;
+            is_Moving = charController.velocity.magnitude > 0.15f; 
+
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Assets/Scripts/FPS Character Script/FPSMouseLook.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSMouseLook : MonoBehaviour
+{
+    public enum RotationAxis {MouseX, MouseY}
+    public RotationAxis axex = RotationAxis.MouseY;
+    private float CurrentSensitity_X = 1.5f;
+    private float CurrentSensitity_Y = 1.5f;
+    private float Sensitity_X = 1.5f;
+    private float Sensitity_Y = 1.5f;
+    private float Rotation_X, Rotation_Y;
+    private float Minimum_X = -360f;
+    private float Max_X = 360f;
+    private float Minimum_Y = -60f;
+    private float Max_Y = 60f;
+    private Quaternion originalRotation;
+    private float mouseSens = 1.7f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalRotation = transform.rotation;
+
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        HandleRotation();
+    }
+
+    float ClampAngle(float angle, float min, float max)
+    {
+        if(angle < -360f)
+        {
+            angle += 360;
+        }
+        if(angle > 360f)
+        {
+            angle -= 360f;
+        }
+        return Mathf.Clamp(angle, min, max);
+
+        void HandleRotation()
+        {
+            if(CurrentSensitity_X != mouseSens || CurrentSensitity_Y != mouseSens)
+            {
+                CurrentSensitity_X = CurrentSensitity_Y = mouseSens;
+            }
+        }
+        Sensitity_X = CurrentSensitity_X;
+        Sensitity_Y = CurrentSensitity_Y;
+        if(axex == RotationAxis.MouseX)
+        {
+            Rotation_X += Input.GetAxis("Mouse X") * Sensitity_X;
+
+            Rotation_X = ClampAngle(Rotation_X, Minimum_X, Max_X);
+            Quaterion xQuaterion = Quaterion.AngleAxis(Rotation_X, Vector3.up);
+            transform.localRotation = originalRotation * xQuaterion;
+        }
+
+        if(axex == RotationAxis.MouseY)
+        {
+            Rotation_Y += Input.GetAxis("Mouse Y") * Sensitity_Y;
+            Rotation_Y = ClampAngle(Rotation_Y, Minimum_Y, Max_Y);
+            Quaterion yQuaterion = Quaterion.AngleAxis(Rotation_Y, Vector3.right);
+            transform.localRotation = originalRotation * yQuaterion;
+        }
+
+    }
+}
